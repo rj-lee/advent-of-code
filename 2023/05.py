9dd8f8df9845d018bbe1e08cb1a6800c902399a1
RJ Lee <rjlee1910@gmail.com>
2025-12-03 12:07:48 +0800

    2023 Day 5: If You Give A Seed A Fertilizer

---
+++ 2023/05.py
@@ -0,0 +1,22 @@
+import re
+
+import utils
+
+lines = utils.get_input(__file__)
+
+
+def part1():
+    sections = lines.split("\n\n")
+    seeds = sections[0].split(": ")[1].split()
+    print(seeds)
+    maps = 
+    print(sections)
+
+
+def part2():
+    pass
+
+
+if __name__ == "__main__":
+    part1()
+    part2()
